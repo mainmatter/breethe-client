4c3f290db466910894e729cff2f7137a07ca8067
Jorge Lainfiesta <jorge.lainfiesta@gmail.com>
2018-03-07 11:53:16 +0100

    Basic store setup

--- src/ui/components/PpmClient/component.ts
+++ src/ui/components/PpmClient/component.ts
@@ -1,5 +1,36 @@
 import Component from '@glimmer/component';
+import { schemaDefinition } from '../../../utils/data/schema';
+import Orbit, { Schema } from '@orbit/data';
+import Store from '@orbit/store';
+import JSONAPIStore from '@orbit/jsonapi';
+import Coordinator from '@orbit/coordinator';
+import { RequestStrategy } from '@orbit/coordinator';
+
+Orbit.fetch = window.fetch.bind(window)
 
 export default class PpmClient extends Component {
+  store = this.setupStore();
 
+  setupStore() {
+    let schema = new Schema(schemaDefinition);
+
+    let store = new Store({ schema });
+    let jsonapi = new JSONAPIStore( { schema });
+    let requestStrategy = new RequestStrategy({
+      source: 'store',
+      on: 'beforeQuery',
+      target: 'jsonapi',
+      action: 'pull',
+      blocking: false
+    });
+
+    const coordinator = new Coordinator({
+      sources: [store, jsonapi],
+      strategies: [requestStrategy]
+    });
+
+    coordinator.activate();
+
+    return store;
+  }
 }
---
+++ src/utils/data/schema.ts
@@ -0,0 +1,32 @@
+import { SchemaSettings } from '@orbit/data';
+import { ModelDefinition } from '@orbit/data';
+
+export const location: ModelDefinition = {
+  attributes: {
+    identifier: { type: 'string' },
+    city: { type: 'string' },
+    country: { type: 'string' },
+    lastUpdated: { type: 'string' },
+    coordinates: { type: 'string' }
+  }
+};
+
+export const measurement: ModelDefinition = {
+  attributes: {
+    identifier: { type: 'string' },
+    city: { type: 'string' },
+    country: { type: 'string' },
+    lastUpdated: { type: 'string' },
+    coordinates: { type: 'string' }
+  },
+  relationships: {
+    location: { type: 'hasOne', model: 'location' }
+  }
+};
+
+export const schemaDefinition:SchemaSettings = {
+  models: {
+    location,
+    measurement
+  }
+};
