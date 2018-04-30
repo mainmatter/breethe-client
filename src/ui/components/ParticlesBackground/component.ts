a95350cd736a4846480b3a7656067b6ec376ba2c
Jorge Lainfiesta <jorge.lainfiesta@gmail.com>
2018-04-30 15:41:22 +0200

    Particles appear gradually

--- src/ui/components/ParticlesBackground/component.ts
+++ src/ui/components/ParticlesBackground/component.ts
@@ -1,11 +1,32 @@
 import Component, { tracked } from '@glimmer/component';
 
 export default class ParticlesBackground extends Component {
+  @tracked
+  lastParticles: number = 0;
+
+  constructor(options) {
+    super(options);
+    this.lastParticles = this.args.index;
+  }
+
   @tracked('args')
   get particles() {
-    let { index } = this.args;
-    let numParticles = index;
-    let particles = Array.from(Array(numParticles).keys());
-    return particles;
+    let numParticles = this.args.index;
+    let { lastParticles } = this;
+    if (numParticles > lastParticles) {
+      let extraParticles = Array.from(Array(numParticles - lastParticles).keys());
+      let particles = Array.from(Array(lastParticles).keys());
+      this.lastParticles = numParticles;
+      return {
+        particles,
+        extraParticles
+      };
+    } else {
+      let particles = Array.from(Array(numParticles).keys());
+      this.lastParticles = numParticles;
+      return {
+        particles
+      };
+    }
   }
 }
