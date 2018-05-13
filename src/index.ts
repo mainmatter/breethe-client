edc8d267fe03764c1a658566c5d9ada8cad3d959
Marco Otte-Witte <marco.otte-witte@simplabs.com>
2018-05-13 21:41:07 +0200

    add sentry

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,26 @@
 import { ComponentManager, setPropertyDidChange } from '@glimmer/component';
 import App from './main';
 
-const app = new App();
-const containerElement = document.getElementById('app');
+let wrapper = (callback) => callback();
+if (typeof Raven !== 'undefined') {
+  wrapper = Raven.context.bind(Raven);
+}
 
-setPropertyDidChange(() => {
-  app.scheduleRerender();
+wrapper(() => {
+  const app = new App();
+  const containerElement = document.getElementById('app');
+
+  setPropertyDidChange(() => {
+    app.scheduleRerender();
+  });
+
+  app.registerInitializer({
+    initialize(registry) {
+      registry.register(`component-manager:/${app.rootName}/component-managers/main`, ComponentManager);
+    }
+  });
+
+  app.renderComponent('PpmClient', containerElement, null);
+
+  app.boot();
 });
-
-app.registerInitializer({
-  initialize(registry) {
-    registry.register(`component-manager:/${app.rootName}/component-managers/main`, ComponentManager);
-  }
-});
-
-app.renderComponent('PpmClient', containerElement, null);
-
-app.boot();
