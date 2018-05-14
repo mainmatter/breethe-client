7cac6c45e8c3dd5174ffe128ae98625b2f7013a0
Marco Otte-Witte <marco.otte-witte@simplabs.com>
2018-05-14 11:33:40 +0200

    use innerHTML instead innerText

--- src/utils/data/restore-cache.ts
+++ src/utils/data/restore-cache.ts
@@ -3,7 +3,7 @@ import Store from '@orbit/store';
 export default function restoreCache(store: Store)  {
   let cacheContainer = document.querySelector('#orbit-main-cache');
   if (cacheContainer) {
-    let data = JSON.parse(cacheContainer.innerText);
+    let data = JSON.parse(cacheContainer.innerHTML);
     data.forEach((record) => {
       store.cache.patch((t) => t.addRecord(record));
     });
