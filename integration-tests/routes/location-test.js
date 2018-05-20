3e0721b6c7322f6b3eae968c723412c2b4a10996
Marco Otte-Witte <marco.otte-witte@simplabs.com>
2018-05-20 21:16:01 +0200

    base SSR tests on document source

--- integration-tests/routes/location-test.js
+++ integration-tests/routes/location-test.js
@@ -4,15 +4,15 @@ const visit = require('../helpers/visit');
 describe('the location route', function() {
   describe('the SSR response', function() {
     it('is rendered', async function() {
-      await visit('/location/2', { waitUntil: 'domcontentloaded' }, async (page) => {
-        let element = await page.$('[data-test-location]');
+      await visit('/location/2', async (page, $response) => {
+        let element = $response('[data-test-location]');
     
-        expect(element).to.be.ok;
+        expect(element.length).to.be.ok;
       });
     });
 
     it('falls back to rendering the empty HTML in case of errors', async function() {
-      await visit('/location/error', { waitUntil: 'domcontentloaded' }, async (page, $response) => {
+      await visit('/location/error', async (page, $response) => {
         expect($response('#app').html()).to.be.empty;
       });
     });
