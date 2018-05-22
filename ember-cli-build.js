951f0e39782a2c0964c80de4f879871273839bc9
Marco Otte-Witte <marco.otte-witte@simplabs.com>
2018-05-22 19:00:48 +0200

    enable SCSS

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -12,6 +12,11 @@ const Funnel = require('broccoli-funnel');
 const Rollup = require('broccoli-rollup');
 const BroccoliCleanCss = require('broccoli-clean-css');
 const Map = require('broccoli-stew').map;
+const GlimmerAnalyzer = require('@css-blocks/glimmer').GlimmerAnalyzer;
+const GlimmerProject = require('@css-blocks/glimmer').Project;
+const Sass = require('node-sass');
+
+const MODULE_CONFIG = require('@glimmer/application-pipeline/dist/lib/broccoli/default-module-configuration.js').default;
 
 const ApiHost = process.env.API_HOST || 'http://localhost:4200';
 
@@ -79,6 +84,7 @@ class PpmGlimmerApp extends GlimmerApp {
   }
 
   package() {
+    return super.package(...arguments);
     let appTree = super.package(...arguments);
     let ssrTree = this.packageSSR();
 
@@ -99,7 +105,19 @@ module.exports = function(defaults) {
     },
     'css-blocks': {
       entry: 'PpmClient',
-      output: 'src/ui/styles/app.css'
+      output: 'src/ui/styles/app.css',
+      getAnalyzer: (app) => {
+        let parserOpts = {
+          preprocessors: {
+            css: function(filePath, content, _options, sourceMap) {
+              let sourceDir = path.dirname(filePath);
+              let result = Sass.renderSync({ data: content, sourceMap: true, outFile: filePath, includePaths: [sourceDir] });
+              return Promise.resolve({ content: result.css.toString(), sourceMap: result.map.toString() });
+            }
+          }
+        };
+        return new GlimmerAnalyzer(new GlimmerProject(app.project.root, MODULE_CONFIG, parserOpts), parserOpts, {});
+      }
     },
     minifyCSS: {
       enabled: process.env.EMBER_ENV === 'production'
