8a228ec0066c164e5d513eed29b2d73938a5669c
Marco Otte-Witte <marco.otte-witte@simplabs.com>
2018-05-27 21:48:22 +0200

    fix SSR for location route

--- integration-tests/routes/location-test.js
+++ integration-tests/routes/location-test.js
@@ -11,6 +11,14 @@ describe('the location route', function() {
       });
     });
 
+    it('includes the measurements data', async function() {
+      await visit('/location/2', async (page, $response) => {
+        let element = $response('[data-test-measurement="PM10"] [data-test-measurement-value="15"]');
+
+        expect(element.length).to.be.ok;
+      });
+    });
+
     it('falls back to rendering the empty HTML in case of errors', async function() {
       await visit('/location/error', async (page, $response) => {
         expect($response('#app').html()).to.be.empty;
