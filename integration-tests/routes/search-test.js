8cdc1fb8d1940b31183491b0c49c278a82d8ccfe
Marco Otte-Witte <marco.otte-witte@simplabs.com>
2018-05-29 18:22:37 +0200

    prerender location search

--- integration-tests/routes/search-test.js
+++ integration-tests/routes/search-test.js
@@ -19,14 +19,6 @@ describe('the search route', function() {
       });
     });
 
-    it('includes the search result', async function() {
-      await visit('/search/Salzburg', async (page, $response) => {
-        let element = $response('[data-test-search-result="Salzburg"]');
-
-        expect(element.length).to.be.ok;
-      });
-    });
-
     it('renders a loader over the search form', async function() {
       await visit('/search/Salzburg', async (page, $response) => {
         let element = $response('[data-test-form-laoder]');
@@ -35,17 +27,45 @@ describe('the search route', function() {
       });
     });
 
-    it('includes the orbit cache', async function() {
-      await visit('/search/Salzburg', async (page, $response) => {
-        let cache = JSON.parse($response('#orbit-main-cache').html());
-
-        expect(cache.orbit.indexOf(r => r.type === 'location' && r.id === 2 && r.attributes.city === 'Salzburg')).to.be.ok;
-      });
-    });
-
     it('falls back to rendering the empty HTML in case of errors', async function() {
       await visit('/search/error', async (page, $response) => {
         expect($response('#app').html()).to.be.empty;
+      });
+    });
+
+    describe('with a search term', function() {
+      it('includes the search result', async function() {
+        await visit('/search/Salzburg', async (page, $response) => {
+          let element = $response('[data-test-search-result="Salzburg"]');
+
+          expect(element.length).to.be.ok;
+        });
+      });
+
+      it('includes the orbit cache', async function() {
+        await visit('/search/Salzburg', async (page, $response) => {
+          let cache = JSON.parse($response('#orbit-main-cache').html());
+
+          expect(cache.orbit.indexOf(r => r.type === 'location' && r.id === 2 && r.attributes.city === 'Salzburg')).to.be.ok;
+        });
+      });
+    });
+
+    describe('with coordinates', function() {
+      it('includes the search result for a e', async function() {
+        await visit('/search/1.11,2.22', async (page, $response) => {
+          let element = $response('[data-test-search-result="Salzburg"]');
+
+          expect(element.length).to.be.ok;
+        });
+      });
+
+      it('includes the orbit cache', async function() {
+        await visit('/search/1.11,2.22', async (page, $response) => {
+          let cache = JSON.parse($response('#orbit-main-cache').html());
+
+          expect(cache.orbit.indexOf(r => r.type === 'location' && r.id === 2 && r.attributes.city === 'Salzburg')).to.be.ok;
+        });
       });
     });
   });
