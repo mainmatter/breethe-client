ac742535ed56e7b27beeb738f81ec8a7538c89f1
Marco Otte-Witte <marco.otte-witte@simplabs.com>
2018-05-29 21:47:12 +0200

    debug router

--- src/ui/components/PpmClient/component.ts
+++ src/ui/components/PpmClient/component.ts
@@ -107,6 +107,7 @@ export default class PpmClient extends Component {
   }
 
   _setupRouting() {
+    console.log(this.appState);
     this.router = new Navigo(this.appState.origin);
 
     this.router
