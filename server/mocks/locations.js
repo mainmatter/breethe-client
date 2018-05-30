751753ed40320a7d6d587134419e51d62faf6fc3
Marco Otte-Witte <marco.otte-witte@simplabs.com>
2018-05-30 15:04:18 +0200

    tweak style a little bit

--- server/mocks/locations.js
+++ server/mocks/locations.js
@@ -48,7 +48,7 @@ const ALL_MEASUREMENTS = [
       value: 15,
       unit: 'ppm',
       parameter: 'pm10',
-      qualityIndex: 'very_high',
+      qualityIndex: 'medium',
       measuredAt: '2018-10-28T16:00:00.000Z'
     },
     relationships: {
@@ -64,7 +64,7 @@ const ALL_MEASUREMENTS = [
       value: 19,
       unit: 'ppm',
       parameter: 'pm25',
-      qualityIndex: 'medium',
+      qualityIndex: 'very_low',
       measuredAt: '2017-11-28T16:00:00.000Z'
     }
   },
--- src/ui/components/FogBackground/component.ts
+++ src/ui/components/FogBackground/component.ts
@@ -4,7 +4,7 @@ export default class FogBackground extends Component {
   @tracked('args')
   get opacityStyle() {
     let { intensity } = this.args;
-    let opacity = Math.pow(2, intensity) / 10;
+    let opacity = Math.pow(2.5, intensity) / 10;
     return `opacity: ${(opacity > 1) ? 1 : opacity}`;
   }
 }
