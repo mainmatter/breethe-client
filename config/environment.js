9817fb3e0943ed4ffcd32f556eec1eccc7edff79
Marco Otte-Witte <marco.otte-witte@simplabs.com>
2019-02-05 22:46:21 +0100

    remove unused module configuration

--- config/environment.js
+++ config/environment.js
@@ -1,43 +1,11 @@
 'use strict';
 
-const moduleConfiguration = {
-  types: {
-    application: { definitiveCollection: 'main' },
-    component: { definitiveCollection: 'components' },
-    'component-test': { unresolvable: true },
-    helper: { definitiveCollection: 'components' },
-    'helper-test': { unresolvable: true },
-    renderer: { definitiveCollection: 'main' },
-    template: { definitiveCollection: 'components' },
-    stylesheet: { definitiveCollection: 'components' }
-  },
-  collections: {
-    main: {
-      types: ['application', 'renderer']
-    },
-    components: {
-      group: 'ui',
-      types: ['component', 'component-test', 'template', 'helper', 'helper-test', 'stylesheet'],
-      defaultType: 'component',
-      privateCollections: ['utils']
-    },
-    utils: {
-      unresolvable: true
-    },
-    styles: {
-      group: 'ui',
-      unresolvable: true
-    },
-  }
-};
-
 module.exports = function(environment) {
   let ENV = {
     environment,
     rootURL: '/',
     modulePrefix: 'breethe',
     locationType: 'auto',
-    moduleConfiguration
   };
 
   return ENV;
