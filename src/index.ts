7029d018de0e8a675d336af2c1a3a3c8005ed9df
Marco Otte-Witte <marco.otte-witte@simplabs.com>
2019-05-09 13:54:23 +0200

    fix tests

--- src/index.ts
+++ src/index.ts
@@ -10,7 +10,7 @@ if (typeof Raven !== 'undefined') {
 wrapper(() => {
   const containerElement = document.getElementById('app');
   const hasSSRBody = document.querySelector('[data-has-ssr-response]');
-  const app = new App({ hasSSRBody });
+  const app = new App({ hasSSRBody: !!hasSSRBody });
 
   setPropertyDidChange(() => {
     app.scheduleRerender();
